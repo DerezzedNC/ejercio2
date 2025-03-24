c53ce7bc4b79944845367ce868d38bf15813fef9
Angel Noh <angelgabrielnohcamargo@gmail.com>
2025-03-24 15:56:54 -0600

    Add files via upload

---
+++ BuscarNombre.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class BuscarNombre
+{
+    static void Main()
+    {
+        string[] nombres = new string[5];
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write($"Ingresa el nombre {i + 1}: ");
+            nombres[i] = Console.ReadLine();
+        }
+
+        Console.Write("\nIngresa el nombre que deseas buscar: ");
+        string buscar = Console.ReadLine();
+
+       
+        bool encontrado = false;
+        for (int i = 0; i < 5; i++)
+        {
+            if (nombres[i].Equals(buscar, StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine($"\nEl nombre '{buscar}' se encuentra en la posición {i}.");
+                encontrado = true;
+                break;
+            }
+        }
+
+        if (!encontrado)
+        {
+            Console.WriteLine($"\nEl nombre '{buscar}' no se encuentra en la lista.");
+        }
+    }
+}
